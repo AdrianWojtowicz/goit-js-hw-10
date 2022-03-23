c12584f2da17b511b335c546108d326bbfe0b4cb
AdrianWojtowicz <adrian.wojtowicz@op.pl>
2022-03-23 22:08:20 +0100

    zad

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,13 +1,14 @@
-export function fetchCountries(name) {
+const fetchCountries = name => {
   return fetch(
-    `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`,
+    `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flag,languages`,
   )
     .then(response => {
       if (!response.ok) {
-        throw new Error(response.status);
+        throw new Error('fetchCountries response error', response.status);
       }
-      console.log(response);
+        console.log('fetchCountries response', response);
       return response.json();
     })
-    .catch(err => console.log(err));
 }
+
+export { fetchCountries };
